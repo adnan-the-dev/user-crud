b7a42fddc7ce14c4329fe64a295939eff8ff33fa
adnan <azizart320480@gmail.com>
2023-08-22 02:30:39 +0500

    chage file

---
+++ src/Table/Table.jsx
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from 'react'
+import "../Table-style/Table.css"
+function Table() {
+
+
+    const [array, setArray] = useState([])
+
+    const [inpuData, setInputData] = useState("")
+    const nameChange = (e) => {
+        setInputData(e.target.value)
+    }
+
+    const [house, setHouse] = useState("")
+    const houseChange = (e) => {
+        setHouse(e.target.value)
+    }
+
+    const [number, setNumber] = useState("")
+    const numberChange = (e) => {
+        setNumber(e.target.value)
+    }
+
+    const [popUp, setPopUp] = useState(false)
+
+
+    function Data() {
+        const userData = {
+            name: inpuData,
+            House: house,
+            number: number
+        }
+        setArray([...array, userData])
+        setInputData("")
+        setHouse("")
+        setNumber("")
+        setPopUp(false)
+    }
+
+
+
+    function deleteData(i) {
+        let total = [array]
+        total.splice(i, 1)
+        setArray(total)
+    }
+
+    function updateData(i) {
+        console.log(array[i], "hwllo");
+        let editData = array[i]
+
+        setInputData(editData.name)
+        setHouse(editData.number)
+        setNumber(editData.number)
+
+        const userEditData = {
+            name: inpuData,
+            house: house,
+            number: number
+        }
+
+        array[i] = userEditData
+
+    }
+
+    return (
+        <>
+            <div className='input-data'>
+
+                {popUp ? (<div>
+                    <input type="text" value={inpuData || ""} placeholder='Enter name' autoComplete='off' name='name' onChange={nameChange} />
+                    <input type="number" value={house || ""} placeholder='Enter house number' name='number' onChange={houseChange} />
+                    <input type="number" value={number || ""} placeholder='Enter number' name='number' onChange={numberChange} />
+                    <button onClick={Data}>Submit</button>
+                </div>) : null
+
+                }
+
+                <button onClick={() => (setPopUp(true))} >Add Data</button>
+                {/* <button onClick={() => (setPopUp(false))}>Hide</button> */}
+            </div>
+
+
+            <table border={1}>
+                <tbody>
+                    <tr>
+                        <th>Name</th>
+                        <th>House</th>
+                        <th>Number</th>
+                        <th>Update</th>
+                        <th>Delete</th>
+                    </tr>
+
+
+                    {
+                        array?.map((item, i) => (
+                            <tr key={i}>
+                                <th>{item.name}</th>
+                                <th>{item.number}</th>
+                                <th>{item.number}</th>
+                                <th><button onClick={() => updateData(i)}>update</button></th>
+                                <th><button onClick={() => deleteData(i)}>Delete</button></th>
+                            </tr>
+                        ))}
+                </tbody>
+            </table >
+
+
+
+
+        </>
+
+    )
+}
+
+export default Table
