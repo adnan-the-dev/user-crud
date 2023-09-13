a922d29adb9a759bc448e63549a85328a6f6c39e
adnan <azizart320480@gmail.com>
2023-09-14 03:42:51 +0500

    card button

--- src/Graphbox/Graph.jsx
+++ src/Graphbox/Graph.jsx
@@ -6,8 +6,11 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 import dayjs from 'dayjs';
-import { Box, Grid } from '@mui/material';
-import { ChildGrid, HeaderBox, ParentBox, ParentGrid, PostionBox, SaleBox } from './Styled-component';
+import { Box, Grid, Typography } from '@mui/material';
+import StickyNote2OutlinedIcon from '@mui/icons-material/StickyNote2Outlined';
+import HighlightOffOutlinedIcon from '@mui/icons-material/HighlightOffOutlined';
+import KeyboardArrowRightOutlinedIcon from '@mui/icons-material/KeyboardArrowRightOutlined';
+import { BoxIcon, ButtonBox, ChildGrid, HeaderBox, IconTypography, NumberBox, ParentBox, ParentGrid, PostionBox, SaleBox } from './Styled-component';
 
 function Graph() {
 
@@ -28,29 +31,87 @@ function Graph() {
                 </PostionBox>
             </HeaderBox>
 
+            {/* *********************Create Box*************************** */}
             <ParentGrid container >
-
-                {/* *********************Create Box*************************** */}
 
                 <ChildGrid item xl={3} lg={3} md={6} sm={12} xs={12} >
                     <SaleBox>
-
-
                         <Box
                             sx={{
                                 bgcolor: 'background.paper',
                                 boxShadow: 1,
-                                borderRadius: 1,
+                                borderRadius: 3,
                                 p: 2,
                                 // minWidth: 300,
-                                width: 200,
-                                height: 200,
+                                width: 180,
+                                height: 180,
                             }}
                         >
-                            <Box sx={{ color: 'text.secondary' }}>Sessions</Box>
+                            <BoxIcon>
+                                <IconTypography>
+                                    <StickyNote2OutlinedIcon />
+                                </IconTypography>
+                                <Box sx={{ color: 'text.secondary' }}>Total orders</Box>
+                            </BoxIcon>
+
+                            <NumberBox>
+                                <Box sx={{ color: 'text.primary', fontSize: 34, fontWeight: 'medium' }}>
+                                    342
+                                </Box>
+                            </NumberBox>
+
+                            <NumberBox>
+                                <Box
+                                    component={TrendingUpIcon}
+                                    sx={{ color: 'success.dark', fontSize: 16, verticalAlign: 'sub' }}
+                                />
+                                <Box
+                                    sx={{
+                                        color: 'success.dark',
+                                        display: 'inline',
+                                        fontWeight: 'medium',
+                                        mx: 0.5,
+                                    }}
+                                >
+                                    18.77%
+                                </Box>
+                                <Box sx={{ color: 'text.secondary', display: 'inline', fontSize: 12 }}>
+                                    vs. last week
+                                </Box>
+                            </NumberBox>
+                        </Box>
+                    </SaleBox>
+                </ChildGrid>
+
+
+
+
+                <ChildGrid item xl={3} lg={3} md={6} sm={12} xs={12} >
+                    <Box
+                        sx={{
+                            bgcolor: 'background.paper',
+                            boxShadow: 1,
+                            borderRadius: 3,
+                            p: 2,
+                            // minWidth: 300,
+                            width: 180,
+                            height: 180,
+                        }}
+                    >
+
+                        <BoxIcon>
+                            <IconTypography>
+                                <StickyNote2OutlinedIcon />
+                            </IconTypography>
+                            <Box sx={{ color: 'text.secondary' }}>Total sales</Box>
+                        </BoxIcon>
+
+                        <NumberBox>
                             <Box sx={{ color: 'text.primary', fontSize: 34, fontWeight: 'medium' }}>
-                                98.3 K
+                                $1290
                             </Box>
+                        </NumberBox>
+                        <NumberBox>
                             <Box
                                 component={TrendingUpIcon}
                                 sx={{ color: 'success.dark', fontSize: 16, verticalAlign: 'sub' }}
@@ -68,42 +129,7 @@ function Graph() {
                             <Box sx={{ color: 'text.secondary', display: 'inline', fontSize: 12 }}>
                                 vs. last week
                             </Box>
-                        </Box>
-                    </SaleBox>
-                </ChildGrid>
-                <ChildGrid item xl={3} lg={3} md={6} sm={12} xs={12} >
-                    <Box
-                        sx={{
-                            bgcolor: 'background.paper',
-                            boxShadow: 1,
-                            borderRadius: 1,
-                            p: 2,
-                            // minWidth: 300,
-                            width: 200,
-                            height: 200,
-                        }}
-                    >
-                        <Box sx={{ color: 'text.secondary' }}>Sessions</Box>
-                        <Box sx={{ color: 'text.primary', fontSize: 34, fontWeight: 'medium' }}>
-                            98.3 K
-                        </Box>
-                        <Box
-                            component={TrendingUpIcon}
-                            sx={{ color: 'success.dark', fontSize: 16, verticalAlign: 'sub' }}
-                        />
-                        <Box
-                            sx={{
-                                color: 'success.dark',
-                                display: 'inline',
-                                fontWeight: 'medium',
-                                mx: 0.5,
-                            }}
-                        >
-                            18.77%
-                        </Box>
-                        <Box sx={{ color: 'text.secondary', display: 'inline', fontSize: 12 }}>
-                            vs. last week
-                        </Box>
+                        </NumberBox>
                     </Box>
                 </ChildGrid>
 
@@ -112,34 +138,45 @@ function Graph() {
                         sx={{
                             bgcolor: 'background.paper',
                             boxShadow: 1,
-                            borderRadius: 1,
+                            borderRadius: 3,
                             p: 2,
                             // minWidth: 300,
-                            minWidth: 200,
-                            height: 200,
+                            width: 180,
+                            height: 180,
                         }}
                     >
-                        <Box sx={{ color: 'text.secondary' }}>Sessions</Box>
-                        <Box sx={{ color: 'text.primary', fontSize: 34, fontWeight: 'medium' }}>
-                            98.3 K
-                        </Box>
-                        <Box
-                            component={TrendingUpIcon}
-                            sx={{ color: 'success.dark', fontSize: 16, verticalAlign: 'sub' }}
-                        />
-                        <Box
-                            sx={{
-                                color: 'success.dark',
-                                display: 'inline',
-                                fontWeight: 'medium',
-                                mx: 0.5,
-                            }}
-                        >
-                            18.77%
-                        </Box>
-                        <Box sx={{ color: 'text.secondary', display: 'inline', fontSize: 12 }}>
-                            vs. last week
-                        </Box>
+
+                        <BoxIcon>
+                            <IconTypography>
+                                <StickyNote2OutlinedIcon />
+                            </IconTypography>
+                            <Box sx={{ color: 'text.secondary' }}>Net sales</Box>
+                        </BoxIcon>
+
+                        <NumberBox>
+                            <Box sx={{ color: 'text.primary', fontSize: 34, fontWeight: 'medium' }}>
+                                $1940
+                            </Box>
+                        </NumberBox>
+                        <NumberBox>
+                            <Box
+                                component={TrendingUpIcon}
+                                sx={{ color: 'success.dark', fontSize: 16, verticalAlign: 'sub' }}
+                            />
+                            <Box
+                                sx={{
+                                    color: 'success.dark',
+                                    display: 'inline',
+                                    fontWeight: 'medium',
+                                    mx: 0.5,
+                                }}
+                            >
+                                18.77%
+                            </Box>
+                            <Box sx={{ color: 'text.secondary', display: 'inline', fontSize: 12 }}>
+                                vs. last week
+                            </Box>
+                        </NumberBox>
                     </Box>
                 </ChildGrid>
 
@@ -149,34 +186,59 @@ function Graph() {
                         sx={{
                             bgcolor: 'background.paper',
                             boxShadow: 1,
-                            borderRadius: 1,
+                            borderRadius: 3,
                             p: 2,
                             // minWidth: 300,
-                            width: 200,
-                            height: 200,
+                            width: 180,
+                            height: 180,
                         }}
                     >
-                        <Box sx={{ color: 'text.secondary' }}>Sessions</Box>
-                        <Box sx={{ color: 'text.primary', fontSize: 34, fontWeight: 'medium' }}>
-                            98.3 K
-                        </Box>
-                        <Box
-                            component={TrendingUpIcon}
-                            sx={{ color: 'success.dark', fontSize: 16, verticalAlign: 'sub' }}
-                        />
-                        <Box
-                            sx={{
-                                color: 'success.dark',
-                                display: 'inline',
-                                fontWeight: 'medium',
-                                mx: 0.5,
-                            }}
-                        >
-                            18.77%
-                        </Box>
-                        <Box sx={{ color: 'text.secondary', display: 'inline', fontSize: 12 }}>
-                            vs. last week
-                        </Box>
+                        <BoxIcon>
+                            <IconTypography isCanceled={1}>
+                                <HighlightOffOutlinedIcon />
+                            </IconTypography>
+                            <Box sx={{ color: 'text.secondary' }}>Cancelled orders</Box>
+                        </BoxIcon>
+                     
+
+                        <NumberBox>
+                            <Box sx={{ color: 'text.primary', fontSize: 34, fontWeight: 'medium' }}>
+                                12
+                            </Box>
+                        </NumberBox>
+                        <NumberBox>
+                            <Box
+                                component={TrendingUpIcon}
+                                sx={{ color: 'success.dark', fontSize: 16, verticalAlign: 'sub' }}
+                            />
+                            <Box
+                                sx={{
+                                    color: 'success.dark',
+                                    display: 'inline',
+                                    fontWeight: 'medium',
+                                    mx: 0.5,
+                                }}
+                            >
+                                18.77%
+                            </Box>
+                            <Box sx={{ color: 'text.secondary', display: 'inline', fontSize: 12 }}>
+                                vs. last week
+                            </Box>
+
+                     {/* *******************Card button section*************** */}
+
+                     <ButtonBox>
+                        <Typography>
+                            View detail
+                        </Typography>
+                        <Typography>
+                            <KeyboardArrowRightOutlinedIcon/>
+                        </Typography>
+                     </ButtonBox>
+
+                           
+
+                        </NumberBox>
                     </Box>
                 </ChildGrid>
 
