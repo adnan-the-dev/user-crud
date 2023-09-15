a1bf0ffca61e29d35f3c6d745a2adc446352d44f
adnan <azizart320480@gmail.com>
2023-09-15 17:13:50 +0500

    card running selling

--- src/Graphbox/Styled-component.jsx
+++ src/Graphbox/Styled-component.jsx
@@ -55,14 +55,14 @@ export const BorderBox = styled(Box)(
     borderTop: isBorder1
       ? "2px solid #8369F1"
       : isBorder2
-      ? "2px solid #FB7269"
-      : isBorder3
-      ? "2px solid #FACC25"
-      : isBorder4
-      ? "2px solid #74C951"
-      : isBorder5
-      ? "2px solid #AFE31F"
-      : "",
+        ? "2px solid #FB7269"
+        : isBorder3
+          ? "2px solid #FACC25"
+          : isBorder4
+            ? "2px solid #74C951"
+            : isBorder5
+              ? "2px solid #AFE31F"
+              : "",
   })
 );
 
@@ -71,14 +71,16 @@ export const BorderBox = styled(Box)(
 }
 
 export const GridContainer = styled(Grid)(() => ({
-    marginTop:"15px"
+  marginTop: "15px"
 }));
 
 export const GridSeling = styled(Grid)(() => ({
-//   border: "2px solid gray",
+  //   border: "2px solid gray",
 }));
 
 
 export const SellingGridBox = styled(Box)(() => ({
-    padding: "6px 60px"
-  }));
+  padding: "6px 60px",
+  // marginTop: "12px"
+
+}));
